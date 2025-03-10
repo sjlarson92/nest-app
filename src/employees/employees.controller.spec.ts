b818b79edd7d6ff1dea2a57f81ba3452cea13c5c
sasha <sjlarson92@gmail.com>
2025-03-10 17:47:40 -0400

    fix tests

--- src/employees/employees.controller.spec.ts
+++ src/employees/employees.controller.spec.ts
@@ -1,20 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
+import { DatabaseService } from '../database/database.service';
 
 describe('EmployeesController', () => {
   let controller: EmployeesController;
+  let service: EmployeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmployeesController],
-      providers: [EmployeesService],
+      providers: [EmployeesService, DatabaseService],
     }).compile();
 
     controller = module.get<EmployeesController>(EmployeesController);
+    service = module.get<EmployeesService>(EmployeesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
--- src/employees/employees.service.spec.ts
+++ src/employees/employees.service.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeesService } from './employees.service';
+import { DatabaseService } from '../database/database.service';
 
 describe('EmployeesService', () => {
   let service: EmployeesService;
+  let databaseService: DatabaseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmployeesService],
+      providers: [EmployeesService, DatabaseService],
     }).compile();
 
     service = module.get<EmployeesService>(EmployeesService);
+    databaseService = module.get<DatabaseService>(DatabaseService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(databaseService).toBeDefined();
   });
 });
